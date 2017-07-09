84f1477582f206d48da29259148fd585ef887ebe
Marcelo Elizeche Landó <melizeche@gmail.com>
2017-07-09 15:39:14 -0400

    Archivo no guardado por error

--- security_issues.js
+++ security_issues.js
@@ -1,5 +1,5 @@
 /**
- * Problemas:
+ * Problema 1:
  *   - Resetea el password de cualquier usuario, como parte del
  *     proceso, pide el email, el hash del password viejo y el
  *     hash de la contraseña vieja
@@ -26,6 +26,7 @@ function reset_all_password_with_token(email, old_passwd_hash, new_passwd) {
 }
 
 /**
+ * Problema 2:
  * El end-point getUsuarios returna el hash del password del usuario
  * consultado, es un problema de seguridad enorme. El mismo hash
  * del password es utilizado para cambiar la contraseña del usuario.
@@ -44,6 +45,7 @@ function reset_all_password_by_email( email, new_passwd ) {
 }
 
 /**
+ * Problema 3:
  * getUsuarios() tiene un problema de SQL injection. Al pasarle estos parametros
  * retorna una lista de todos los usuarios del sistema, con los hashes de sus
  * password.
@@ -62,3 +64,25 @@ function reset_all_password( new_pass ) {
             });
         });
 }
+
+/**
+ * Problema 4:
+ * Al crear y validar usuario el hash se genera en el cliente y no en el servidor, además de ser un riesgo
+ * de seguridad esto imposibilita validar la longitud de contraseñas ademas de confiar en la información
+ * que llega desde el cliente
+ *
+ * Solucionar:
+ *   - Aplicar el algoritmo de hashing en el servidor (Solucionado en nuestro API Django)
+ */
+
+ /**
+ * Problema 5:
+ * Se utiliza un algoritmo de hashing(MD5) totalmente obsoleto e inseguro, facilmente reversible
+ * en varios servicios online o en tablas de hash, solo falta 'googlear' el hash(que devuelve el API)
+ *
+ * Solucion:
+ *   - Usar un algoritmo de hashing como BCrypt o SHA356 con Salteo e
+ *     iteraciones(Solucionado en nuestro API Django)
+ */
+
+
